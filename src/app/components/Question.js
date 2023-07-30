f382578c10c6f9457b428cbcd9b6ad59142b1f04
maycon <maycondsn12@gmail.com>
2023-07-29 20:32:08 -0400

    add hover for question block

--- src/app/components/Question.js
+++ src/app/components/Question.js
@@ -5,13 +5,15 @@ import {AiOutlineEye} from 'react-icons/ai'
 
 export default function Question() {
     return (
-        <Link href='/pages/question' className='border-b border-black mx-6 flex text-left'>
+        <Link href='/pages/question' className='border-b border-black mx-6 flex text-left
+        
+        '>
             <div className='grid gap-2 p-6'>
               <div className='flex gap-5 text-xs text-teal-600'>
                 <span className='flex gap-2'><BiCheck className='text-base'/>0 answers</span>
                 <span className='flex gap-2'><AiOutlineEye className='text-base'/> 0 views</span>
               </div>
-              <div>
+              <div className="hover:text-teal-900">
                 <h1 className='text-2xl font-semibold pb-2'>Lorem ipsum sit dolot amet</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc blandit, massa nec dictum pretium, purus ligula imperdiet lacus, nec sollicitudin quam ...</p>
               </div>
